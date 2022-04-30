3bb2572a85dbc61526becda99f72d1beeba00179
wlto <to_william@icloud.com>
2022-04-30 19:26:52 -0400

    feat: using explicit ignore files

--- src/example.js
+++ src/example.js
@@ -1,11 +1,11 @@
-const username = 'wlto';
-typeof username === 'string';
+const username = "wlto";
+typeof username === "string";
 
-if (!('serviceWorker' in navigator)) {
+if (!("serviceWorker" in navigator)) {
   // you have an old browser
 }
 
-const greeting = 'hello';
+const greeting = "hello";
 console.log(`${greeting} world!`);
 
-[1, 2, 4].forEach(x => console.log(x));
+[1, 2, 4].forEach((x) => console.log(x));
