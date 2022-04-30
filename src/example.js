87a7a7a352529605fa5a4a377257436c538c05d8
wlto <to_william@icloud.com>
2022-04-30 17:00:30 -0400

    initial commit

---
+++ src/example.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const username = 'wlto'
+typeof username === 'strng'
+
+if (!'serviceWorker' in navigator) {
+  // you have an old browser
+}
+
+const greeting = 'hello'
+console.log('${greeting} world!')
+[1, 2, 4].forEach(x => console.log(x))
